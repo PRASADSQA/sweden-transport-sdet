826da0605a4e34e78cb993ca293d4c0fffd3df9f
Prasad Ranasinghe <janendrar@gmail.com>
2026-01-05 05:39:51 +0100

    add playwright config and pytest

---
+++ playwright.config.py
@@ -0,0 +1,7 @@
+from playwright.sync_api import BrowserType
+import os
+
+BASE_URL = "https://www.sl.se"  # Example: Sweden Public Transport (SL)
+
+def pytest_configure(config):
+    config.option.base_url = BASE_URL
